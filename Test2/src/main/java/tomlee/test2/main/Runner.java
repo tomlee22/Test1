95da39f021563d3c2763ab6deb5959853787acc1
Tom <tomlee22@gmail.com>
2019-11-25 21:46:35 -0800

    Initial Commit.

---
+++ Test2/src/main/java/tomlee/test2/main/Runner.java
@@ -0,0 +1,12 @@
+package tomlee.test2.main;
+
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+
+public class Runner {
+    public static void main(String[] args) {
+        List<Long> nums = ImmutableList.of(1L);
+        System.out.println(nums);
+    }
+}
